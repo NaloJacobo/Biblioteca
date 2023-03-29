e274c7cfe079b45a91597fc2d6be0729d8cb0de4
NaloJacobo <88952388+NaloJacobo@users.noreply.github.com>
2023-03-29 01:23:50 -0600

    Add files via upload

---
+++ Biblioteca/src/mx/uaemex/dao/AlumnoDao.java
@@ -0,0 +1,14 @@
+package mx.uaemex.dao;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Nalo
+ */
+public class AlumnoDao {
+    
+}
